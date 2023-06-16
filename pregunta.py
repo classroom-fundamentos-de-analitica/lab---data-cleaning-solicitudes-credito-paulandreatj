24b75695233255270484caa630ff11b395c785bd
Paula Taborda <ptaborda@unal.edu.co>
2023-06-16 11:53:53 -0500

    Solucion Taller 5

--- pregunta.py
+++ pregunta.py
@@ -44,5 +44,4 @@ def clean_data():
             subset=["sexo", "tipo_de_emprendimiento", 'idea_negocio', 'barrio', 'estrato', 'comuna_ciudadano', 
             'fecha_de_beneficio','monto_del_credito', 'línea_credito'],
         )
-
     return df
